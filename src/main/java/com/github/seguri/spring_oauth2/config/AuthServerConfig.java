9383bd2614da753a3ffbd7d9d510bd7aea390ff4
Marco Seguri <seguri@users.noreply.github.com>
2020-12-12 21:30:11 +0100

    Add client management (grant_type=password)

--- src/main/java/com/github/seguri/spring_oauth2/config/AuthServerConfig.java
+++ src/main/java/com/github/seguri/spring_oauth2/config/AuthServerConfig.java
@@ -3,6 +3,7 @@ package com.github.seguri.spring_oauth2.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
@@ -16,5 +17,15 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
   @Override
   public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
     endpoints.authenticationManager(authenticationManager);
+  }
+
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    clients
+        .inMemory()
+        .withClient("client")
+        .secret("secret")
+        .authorizedGrantTypes("password")
+        .scopes("read");
   }
 }
