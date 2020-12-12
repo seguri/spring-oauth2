e79a5eeeb2db0636550a2236bb4d1a2f6c4650f0
Marco Seguri <seguri@users.noreply.github.com>
2020-12-12 18:51:36 +0100

    Add spotless plugin

--- src/test/java/com/github/seguri/spring_oauth2/SpringOauth2ApplicationTests.java
+++ src/test/java/com/github/seguri/spring_oauth2/SpringOauth2ApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringOauth2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
