b885ffadfb818bb8b19006611e2cf964e9fc189e
Marco Seguri <seguri@users.noreply.github.com>
2020-12-12 21:12:12 +0100

    Add user management

---
+++ src/main/java/com/github/seguri/spring_oauth2/config/AuthServerConfig.java
@@ -0,0 +1,20 @@
+package com.github.seguri.spring_oauth2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+  @Autowired private AuthenticationManager authenticationManager;
+
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+    endpoints.authenticationManager(authenticationManager);
+  }
+}
---
+++ src/main/java/com/github/seguri/spring_oauth2/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.github.seguri.spring_oauth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/** User management. */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Bean
+  @Override
+  protected UserDetailsService userDetailsService() {
+    var uds = new InMemoryUserDetailsManager();
+    var u = User.withUsername("john").password("12345").authorities("read").build();
+    uds.createUser(u);
+    return uds;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
+  }
+
+  /**
+   * Link the User Management to the Authorization Server by exposing the AuthenticationManager in
+   * the Spring context as a bean.
+   */
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+}
