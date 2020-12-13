0a33181cb1bee5b75fa35e474e131510b35cd42f
Marco Seguri <seguri@users.noreply.github.com>
2020-12-13 18:45:30 +0100

    Update WebSecurityConfig to load users from the database

---
+++ src/main/java/com/github/seguri/spring_oauth2/domain/service/JpaUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.github.seguri.spring_oauth2.domain.service;
+
+import com.github.seguri.spring_oauth2.domain.SimpleSecurityUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * This is the implementation of UserDetailsService that Spring will load into its context.
+ */
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+  @Autowired private UserService userService;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return userService
+        .findByUsername(username)
+        .map(SimpleSecurityUser::new)
+        .orElseThrow(() -> new UsernameNotFoundException(username + ": no such user"));
+  }
+}
