0b8c9bf6a3e26dba0caf495721acea0ca5f19409
Marco Seguri <seguri@users.noreply.github.com>
2020-12-13 16:45:02 +0100

    Add endpoints to retrieve users and clients

--- src/main/java/com/github/seguri/spring_oauth2/domain/application/rest/ClientController.java
+++ src/main/java/com/github/seguri/spring_oauth2/domain/application/rest/ClientController.java
@@ -2,10 +2,12 @@ package com.github.seguri.spring_oauth2.domain.application.rest;
 
 import com.github.seguri.spring_oauth2.domain.Client;
 import com.github.seguri.spring_oauth2.domain.service.ClientService;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +21,14 @@ public class ClientController {
 
   @Autowired private ClientService clientService;
 
+  @GetMapping
+  public List<Client> getClients() {
+    return clientService.findAll();
+  }
+
   @PostMapping(consumes = "application/json")
   @ResponseStatus(HttpStatus.CREATED)
-  public Client postTaco(@RequestBody Client client) {
+  public Client createClient(@RequestBody Client client) {
     Client saved = clientService.save(client);
     LOGGER.info("Saved '{}'", saved);
     return saved;
--- src/main/java/com/github/seguri/spring_oauth2/domain/application/rest/UserController.java
+++ src/main/java/com/github/seguri/spring_oauth2/domain/application/rest/UserController.java
@@ -2,10 +2,12 @@ package com.github.seguri.spring_oauth2.domain.application.rest;
 
 import com.github.seguri.spring_oauth2.domain.User;
 import com.github.seguri.spring_oauth2.domain.service.UserService;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +21,14 @@ public class UserController {
 
   @Autowired private UserService userService;
 
+  @GetMapping
+  public List<User> getUsers() {
+    return userService.findAll();
+  }
+
   @PostMapping(consumes = "application/json")
   @ResponseStatus(HttpStatus.CREATED)
-  public User postTaco(@RequestBody User user) {
+  public User createUser(@RequestBody User user) {
     User saved = userService.save(user);
     LOGGER.info("Saved '{}'", saved);
     return saved;
