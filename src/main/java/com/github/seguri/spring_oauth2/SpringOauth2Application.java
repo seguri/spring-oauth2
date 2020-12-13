d5a100d5cddf0bae34a6dad6662e55905b3730e6
Marco Seguri <seguri@users.noreply.github.com>
2020-12-13 13:50:57 +0100

    Bootstrap default user and client

--- src/main/java/com/github/seguri/spring_oauth2/SpringOauth2Application.java
+++ src/main/java/com/github/seguri/spring_oauth2/SpringOauth2Application.java
@@ -1,12 +1,30 @@
 package com.github.seguri.spring_oauth2;
 
+import com.github.seguri.spring_oauth2.domain.service.ClientService;
+import com.github.seguri.spring_oauth2.domain.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringOauth2Application {
+public class SpringOauth2Application implements CommandLineRunner {
+  private static final Logger LOGGER = LoggerFactory.getLogger(SpringOauth2Application.class);
+
+  @Autowired UserService userService;
+  @Autowired ClientService clientService;
 
   public static void main(String[] args) {
     SpringApplication.run(SpringOauth2Application.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    userService.findByUsername("john").ifPresentOrElse(user -> {}, userService::saveTestUser);
+    clientService
+        .findByClientId("client")
+        .ifPresentOrElse(client -> {}, clientService::saveTestClient);
   }
 }
