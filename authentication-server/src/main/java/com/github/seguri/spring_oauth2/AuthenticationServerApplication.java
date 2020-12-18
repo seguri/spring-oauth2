92f81b4a27c321c10a22e54792d0cd10e20e6ac3
Marco Seguri <seguri@users.noreply.github.com>
2020-12-18 23:29:40 +0100

    Rename main application

--- authentication-server/src/main/java/com/github/seguri/spring_oauth2/AuthenticationServerApplication.java
+++ authentication-server/src/main/java/com/github/seguri/spring_oauth2/AuthenticationServerApplication.java
@@ -2,26 +2,26 @@ package com.github.seguri.spring_oauth2;
 
 import com.github.seguri.spring_oauth2.domain.service.ClientService;
 import com.github.seguri.spring_oauth2.domain.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringOauth2Application implements CommandLineRunner {
-  private static final Logger LOGGER = LoggerFactory.getLogger(SpringOauth2Application.class);
+public class AuthenticationServerApplication implements CommandLineRunner {
+  private final UserService userService;
+  private final ClientService clientService;
 
-  @Autowired UserService userService;
-  @Autowired ClientService clientService;
+  public AuthenticationServerApplication(UserService userService, ClientService clientService) {
+    this.userService = userService;
+    this.clientService = clientService;
+  }
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringOauth2Application.class, args);
+    SpringApplication.run(AuthenticationServerApplication.class, args);
   }
 
   @Override
-  public void run(String... args) throws Exception {
+  public void run(String... args) {
     userService.findByUsername("john").ifPresentOrElse(user -> {}, userService::saveTestUser);
     clientService
         .findByClientId("client")
