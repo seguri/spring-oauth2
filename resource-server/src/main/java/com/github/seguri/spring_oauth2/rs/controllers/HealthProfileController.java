228b2175a4acbae89c806fbe5c872d16a2171021
Marco Seguri <seguri@users.noreply.github.com>
2020-12-24 20:16:05 +0100

    Delete unnecessary code

--- resource-server/src/main/java/com/github/seguri/spring_oauth2/rs/controllers/HealthProfileController.java
+++ resource-server/src/main/java/com/github/seguri/spring_oauth2/rs/controllers/HealthProfileController.java
@@ -2,8 +2,13 @@ package com.github.seguri.spring_oauth2.rs.controllers;
 
 import com.github.seguri.spring_oauth2.rs.entities.HealthProfile;
 import com.github.seguri.spring_oauth2.rs.services.HealthProfileService;
-// TODO import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/profile")
@@ -26,8 +31,6 @@ public class HealthProfileController {
   }
 
   @DeleteMapping("/{username}")
-  // TODO re-enable after adding security
-  // public void deleteHealthProfile(@PathVariable String username, Authentication a) {
   public void deleteHealthProfile(@PathVariable String username) {
     healthProfileService.deleteHealthProfile(username);
   }
