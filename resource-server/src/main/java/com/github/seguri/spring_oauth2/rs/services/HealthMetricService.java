3bab969d46a06a0e246de29f46c30f1229b889cd
Marco Seguri <seguri@users.noreply.github.com>
2020-12-26 15:23:23 +0100

    Refactor HealthMetricService

--- resource-server/src/main/java/com/github/seguri/spring_oauth2/rs/services/HealthMetricService.java
+++ resource-server/src/main/java/com/github/seguri/spring_oauth2/rs/services/HealthMetricService.java
@@ -1,12 +1,10 @@
 package com.github.seguri.spring_oauth2.rs.services;
 
 import com.github.seguri.spring_oauth2.rs.entities.HealthMetric;
-import com.github.seguri.spring_oauth2.rs.entities.HealthProfile;
 import com.github.seguri.spring_oauth2.rs.exceptions.NonExistentHealthProfileException;
 import com.github.seguri.spring_oauth2.rs.repositories.HealthMetricRepository;
 import com.github.seguri.spring_oauth2.rs.repositories.HealthProfileRepository;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,37 +23,25 @@ public class HealthMetricService {
   }
 
   public void addHealthMetric(HealthMetric healthMetric) {
-    Optional<HealthProfile> profile =
-        healthProfileRepository.findHealthProfileByUsername(
-            healthMetric.getProfile().getUsername());
-
-    profile.ifPresentOrElse(
-        p -> {
-          healthMetric.setProfile(p);
-          healthMetricRepository.save(healthMetric);
-        },
-        () -> {
-          throw new NonExistentHealthProfileException("The profile doesn't exist");
-        });
-
-    ;
+    healthProfileRepository
+        .findHealthProfileByUsername(healthMetric.getProfile().getUsername())
+        .ifPresentOrElse(
+            p -> {
+              healthMetric.setProfile(p);
+              healthMetricRepository.save(healthMetric);
+            },
+            NonExistentHealthProfileException::throwDefault);
   }
 
-  /**
-   * `authentication.principal` contains the JWT. Is is only signed, not encrypted. You can see it's
-   * content at jwt.io
-   */
   public List<HealthMetric> findHealthMetricHistory(String username) {
     return healthMetricRepository.findHealthMetricHistory(username);
   }
 
   public void deleteHealthMetricForUser(String username) {
-    Optional<HealthProfile> profile = healthProfileRepository.findHealthProfileByUsername(username);
-
-    profile.ifPresentOrElse(
-        healthMetricRepository::deleteAllForUser,
-        () -> {
-          throw new NonExistentHealthProfileException("The profile doesn't exist");
-        });
+    healthProfileRepository
+        .findHealthProfileByUsername(username)
+        .ifPresentOrElse(
+            healthMetricRepository::deleteAllForUser,
+            NonExistentHealthProfileException::throwDefault);
   }
 }
