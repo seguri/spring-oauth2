02763d6fe6c5ddd49364eef395cf8733ef9e490a
Marco Seguri <seguri@users.noreply.github.com>
2020-12-28 21:51:15 +0100

    Delete unnecessary code

--- authentication-server/src/main/java/com/github/seguri/spring_oauth2/as/AuthenticationServerApplication.java
+++ authentication-server/src/main/java/com/github/seguri/spring_oauth2/as/AuthenticationServerApplication.java
@@ -1,19 +1,10 @@
 package com.github.seguri.spring_oauth2.as;
 
-import com.github.seguri.spring_oauth2.as.domain.service.ClientService;
-import com.github.seguri.spring_oauth2.as.domain.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthenticationServerApplication {
-  private final UserService userService;
-  private final ClientService clientService;
-
-  public AuthenticationServerApplication(UserService userService, ClientService clientService) {
-    this.userService = userService;
-    this.clientService = clientService;
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(AuthenticationServerApplication.class, args);
